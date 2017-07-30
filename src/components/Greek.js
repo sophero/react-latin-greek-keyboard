d9b9560de97a0cc2bcfca6ef91f7ab93f4a91b23
Sophia Di <backwardsxmarathon@gmail.com>
2017-07-30 16:52:52 -0400

    Clean up comments.

--- src/components/Greek.js
+++ src/components/Greek.js
@@ -36,12 +36,8 @@ class Greek extends Component {
 
   updateText(event) {
     let newText = event.target.value;
-    // let ind = 1 + this.firstDiffIndex(newText, this.state.text);
     let ind = this.cursorPlacer(newText, this.state.text);
-    console.log('ind',ind);
-
     for (let k = 0; k < charMappings.length; k++) {
-      // newText = this.replaceKeysWithValues(charMappings[k], newText);
       for (var p in charMappings[k]) {
         if (charMappings[k].hasOwnProperty(p)) {
           if (newText.indexOf(p) > -1) {
@@ -49,8 +45,6 @@ class Greek extends Component {
             ind -= lenDiff;
           }
           newText = newText.replace(p, charMappings[k][p]);
-          // can't have eg. 'a)' in RegExp
-          // string = string.replace(RegExp(p), obj[p]);
         }
       }
     }
@@ -60,30 +54,15 @@ class Greek extends Component {
     });
   }
 
-  // replaceKeysWithValues(obj, string) {
-  //   for (var p in obj) {
-  //     if (obj.hasOwnProperty(p)) {
-  //       let lenDiff = obj[p].length - p.length;
-  //       string = string.replace(p, obj[p]);
-  //       // can't have eg. 'a)' in RegExp
-  //       // string = string.replace(RegExp(p), obj[p]);
-  //     }
-  //   }
-  //   return string;
-  // }
-
   cursorPlacer(newStr, oldStr) {
     if (oldStr === newStr) {
       return oldStr.length;
     }
-    let len0 = oldStr.length;
-    let len1 = newStr.length;
-    let maxLen = Math.max(len0, len1);
-    let lenDiff = len1 - len0;
-    console.log('lenDiff',lenDiff);
+    let lenDiff = newStr.length - oldStr.length;
+    let maxLen = Math.max(newStr.length, oldStr.length);
+
     for (let k = 0; k < maxLen; k++) {
       if (oldStr[k] !== newStr[k]) {
-        console.log('k', k);
         if (lenDiff > 1) {
           return k + lenDiff;
         } else if (lenDiff >= 0) {
@@ -91,36 +70,6 @@ class Greek extends Component {
         } else {
           return k;
         }
-      }
-    }
-
-  }
-
-  firstDiffIndex(str1, str2) {
-    if (str1 === str2) {
-      return false;
-    }
-    let len1 = str1.length;
-    let len2 = str2.length;
-    let maxLen = Math.max(len1, len2)
-    let lenDiff = Math.abs(len1 - len2);
-    console.log(lenDiff);
-    // if (lenDiff > 1) {
-    //   // str1 = str1.split("").reverse().join("");
-    //   // str2 = str2.split("").reverse().join("");
-    //   for (let k = maxLen; k > -1; k--) {
-    //     if (str1[k] !== str2[k]) {
-    //       return k;
-    //     }
-    //   }
-    // }
-    for (let k = 0; k < maxLen; k++) {
-      if (str1[k] !== str2[k]) {
-        if (lenDiff > 1) {
-          return k;
-        }
-        //   return len1 - k + lenDiff - 2;
-        return k;
       }
     }
   }
